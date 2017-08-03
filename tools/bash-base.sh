55835ab647823378a1d4331e1ce8e2a22788269f
Charlie Kim <kimcharli@gmail.com>
2017-08-03 00:10:18 -0400

    added bash-bash.sh as a bash boiler plate

---
+++ tools/bash-base.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# Bash3 Boilerplate. Copyright (c) 2014, kvz.io
+
+set -o errexit
+set -o pipefail
+set -o nounset
+# set -o xtrace
+
+# Set magic variables for current file & dir
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+__file="${__dir}/$(basename "${BASH_SOURCE[0]}")"
+__base="$(basename ${__file} .sh)"
+__root="$(cd "$(dirname "${__dir}")" && pwd)" # <-- change this as it depends on your app
+
+arg1="${1:-}"
+
+echo __dir = ${__dir}
+echo __file = ${__file}
+echo __base = ${__base}
+echo __root = ${__root}
+echo arg1 = ${arg1}
+
